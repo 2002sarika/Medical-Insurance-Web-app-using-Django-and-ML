35462bf0c8dc27a2b7f00ac574368a4fd9fe5548
2002sarika <71333456+2002sarika@users.noreply.github.com>
2022-01-24 12:00:00 +0530

    Update settings.py

--- insurance/settings.py
+++ insurance/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7gzm)et(35jqk3ejghnl6_%kwf7km6+jy06nd6+6rimmca5w94'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
