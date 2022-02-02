e64bd6d4ee76c33db1472c6b28e30a1884382ecf
2002sarika <71333456+2002sarika@users.noreply.github.com>
2022-02-02 17:26:31 +0530

    Update settings.py

--- insurance/settings.py
+++ insurance/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7gzm)et(35jqk3ejghnl6_%kwf7km6+jy06nd6+6rimmca5w94'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
